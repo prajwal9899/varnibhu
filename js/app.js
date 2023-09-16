0d3b11ea1e20fc63aa6e9805a1493e38f479ea1d
Prajwal Gadge <prajwal@masycoda.com>
2023-09-16 08:23:12 +0530

    fic evnts ttitle

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-const sendBtn = document
-  .getElementById("sendBtn")
-  .addEventListener("click", () => {});
-
-function sendEmail() {}
-
-// const navBtn = document
-//   .getElementById("nav-check")
-//   .addEventListener("click", (event) => {
-//     console.log(document.getElementById("nav-check").checked);
-//   });
-
 document.getElementById("home_links1").addEventListener("click", (event) => {
   if (document.getElementById("nav-check").checked == true) {
     document.getElementById("nav-check").checked = false;
