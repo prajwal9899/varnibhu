c5b0607f5fcf8843c170b05fd2eee48c9b109f20
Prajwal Gadge <prajwal@masycoda.com>
2023-09-16 10:31:26 +0530

    Hadle email send

--- js/app.js
+++ js/app.js
@@ -26,8 +26,51 @@ const sendBtn = document
   .getElementById("sendBtn")
   .addEventListener("click", () => {
     let dispaly = document.getElementById("response");
-    dispaly.innerHTML = "Thanks for submitting";
-    setTimeout(() => {
-      dispaly.innerHTML = "";
-    }, 4000);
+
+    let name = document.getElementById("name").value;
+    let last_name = document.getElementById("last_name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    emailjs.init("vbFsDRgAQuM2igArp");
+    var templateParams = {
+      name: `${name} ${last_name}`,
+      email: email,
+      message: message,
+    };
+
+    emailjs.send("service_varnibhu", "template_qp5mg9i", templateParams).then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        dispaly.innerHTML = "Thanks for submitting";
+        document.getElementById("name").value = ""
+        last_name = ""
+        email = ""
+        message = ""
+        setTimeout(() => {
+          dispaly.innerHTML = "";
+        }, 4000);
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+    );
   });
+
+// const sendEmail = () => {
+//   emailjs.init("z-lsdP50vYBdAFZisbSQd");
+//   var templateParams = {
+//     name: "James",
+//     email: "prajwalgadge9899@gmail.com",
+//     message: "hello from varnibhu",
+//   };
+
+//   emailjsForm.send("service_varnibhu", "template_4hkq57r", templateParams).then(
+//     function (response) {
+//       console.log("SUCCESS!", response.status, response.text);
+//     },
+//     function (error) {
+//       console.log("FAILED...", error);
+//     }
+//   );
+// };
