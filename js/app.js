0df5cfe4b8cdaae727f56bcee8dd17be09807be6
Prajwal Gadge <prajwal@masycoda.com>
2023-09-16 08:15:04 +0530

    fix navbar

--- js/app.js
+++ js/app.js
@@ -3,3 +3,33 @@ const sendBtn = document
   .addEventListener("click", () => {});
 
 function sendEmail() {}
+
+// const navBtn = document
+//   .getElementById("nav-check")
+//   .addEventListener("click", (event) => {
+//     console.log(document.getElementById("nav-check").checked);
+//   });
+
+document.getElementById("home_links1").addEventListener("click", (event) => {
+  if (document.getElementById("nav-check").checked == true) {
+    document.getElementById("nav-check").checked = false;
+  }
+});
+
+document.getElementById("home_links2").addEventListener("click", (event) => {
+  if (document.getElementById("nav-check").checked == true) {
+    document.getElementById("nav-check").checked = false;
+  }
+});
+
+document.getElementById("home_links3").addEventListener("click", (event) => {
+  if (document.getElementById("nav-check").checked == true) {
+    document.getElementById("nav-check").checked = false;
+  }
+});
+
+document.getElementById("home_links4").addEventListener("click", (event) => {
+  if (document.getElementById("nav-check").checked == true) {
+    document.getElementById("nav-check").checked = false;
+  }
+});
